b73fb7f997717cb3ad9d604b63c89a3379c7e494
pvip-nitin <121781989+pvip-nitin@users.noreply.github.com>
2023-03-14 00:32:57 +0530

    Update MUL.sv

--- MUL.sv
+++ MUL.sv
@@ -4,7 +4,8 @@ module full_adder #(parameter N=16)
   (
     input [N-1:0] A,
     input [N-1:0] B,
-    output [N-1:0] O
+    output [N-1:0] O,
+    output of
 );
   
   
@@ -18,26 +19,39 @@ module MUL#(parameter N=16)(
   output [2*N-1:0] O
 );
   
-  wire [N][  N-1:0] temp1;// [N];
+  wire [N][2*N-1:0] temp1;// [N];
   wire [N][2*N-1:0] temp2;// [N];
-  
   wire [N][2*N-1:0] temp3;// [N];
   
-  //always_comb begin
-  assign	temp1[0] = B[0] ? A : 0;//& {N{B[0]}};
-  assign	temp2[0] = temp1[0] << 0;
+  
+  
+  
+  zExtend#(N) zE(
+    .i(A),
+    .o(temp[0])
+  );
+  //Left shift;
+  assign	temp1[0] = A;
+  //if valid
+  assign	temp2[0] = B[0] ? temp1[0] : 0;
+  //Add
   assign	temp3[0] = temp2[0];
-  //end
   
   genvar i;
   
   generate
     for(i=1; i<=N-1; i++)begin
-      assign temp1[i] = B[i] ? A : 0;
-      assign temp2[i] = temp1[i] << i;
+      
+      Lshift#(2*N) Ls(
+        .i(temp1[i-1]),
+        .o(temp1[i])
+      );
+  
+      assign temp2[i] = B[i] ? temp1[i] : 0;
+      
       full_adder#(2*N) fa(
-        .A(temp3[i-1]),
-        .B(temp2[i]),
+        .A(temp2[i]),
+        .B(temp3[i-1]),
         .O(temp3[i])
       );
     end
@@ -45,4 +59,25 @@ module MUL#(parameter N=16)(
 
   assign O = temp3[N-1];
   
+endmodule
+
+
+module Lshift#(parameter N=16)(
+  input [N-1:0] i,
+  output [N-1:0] o
+);
+  
+  assign o[0] = 1'b0;
+  assign o[N-1:1] = i[N-2:0];
+  
+endmodule
+  
+module zExtend#(parameter N=16)(
+  input [N-1:0] i,
+  output [2*N-1:0] o
+);
+  
+  assign o[N-1:0] = i;
+  assign o[2*N-1:N] = 0;
+    
 endmodule
